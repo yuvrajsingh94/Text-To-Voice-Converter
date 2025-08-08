b326950d5be592827d458d387d30add8e01f47ab
Aman Chauhan <amanchauhan22026@gmail.com>
2025-08-08 18:05:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 let speech = new SpeechSynthesisUtterance();
 let voices = [];
 let voiceSelect = document.querySelector("select");
+
 window.speechSynthesis.onvoiceschanged = () => {
   voices = window.speechSynthesis.getVoices();
   speech.voice = voices[0];
-  voices.forEach(
-    (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
-  );
+  voices.forEach((voice, i) => {
+    voiceSelect.options[i] = new Option(voice.name, i);
+  });
 };
 
 document.querySelector("button").addEventListener("click", () => {
-  speech.text = document.querySelector("textarea").value;
+  window.speechSynthesis.cancel(); // added this to avoid any ongoing speech
+  speech.text = document.querySelector("textarea").value; // check text if it's empty
+  if (!speech.text.trim()) {
+    alert("Please enter some text to speak.");
+    return;
+  }
   window.speechSynthesis.speak(speech);
 });
-voiceSelect.addEventListener("change", ()=>{
-    speech.voice = voices[voiceSelect.value];
+
+voiceSelect.addEventListener("change", () => {
+  speech.voice = voices[voiceSelect.value];
 });
